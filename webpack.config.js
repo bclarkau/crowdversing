679c0c2e4c84c7f2518629a86899a5879f6e602d
Ben <benclark001@gmail.com>
2020-07-09 17:21:34 +1000

    add webpack config, build scripts and base components

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = env => {
+	return  {
+		entry: [
+			"@babel/polyfill", // polyfill must be included before the app entry point
+			"./src/index.js"
+		],
+		output: {
+			path: path.join(__dirname, "/dist"),
+			filename: "app.bundle.js",
+			publicPath: '/',
+		},
+		devServer: {
+			historyApiFallback: true,
+		},
+		resolve: {
+			extensions: [".js"]
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader"
+					},
+				},
+				{
+					test: /\.(sa|sc|c)ss$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+							options: {
+								hmr: true,
+							},
+						},
+						'css-loader',
+						'sass-loader',
+					],
+				}
+			]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: '[name].css',
+				chunkFilename: '[id].css'
+			}),
+			new HtmlWebpackPlugin({
+				template: "./src/index.html"
+			})
+		]
+	}
+};
