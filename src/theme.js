2a8f2f3653f5078f0b7513cc82656e8ff68fe5f0
Ben <benclark001@gmail.com>
2020-08-03 17:01:35 +1000

    change incorrect state avatar colour

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ export const theme = {
 		default: ["#ffbe0b","#fb5607","#ff006e","#8338ec","#3a86ff"],
 		lost: '#EEE',
 		correct: 'green',
-		incorrect: 'red',
+		incorrect: '#EEE',
 		answered: 'orange',
 		unanswered: 'lightgrey'
 	}
